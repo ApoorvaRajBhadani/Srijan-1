0a8654c53ea672392fd9dd065f93a7f479a51c9a
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-02-04 18:16:23 +0530

    Added items ad image in drawer

--- app/src/main/java/com/example/lenovo/srijan/MainActivity.java
+++ app/src/main/java/com/example/lenovo/srijan/MainActivity.java
@@ -5,10 +5,13 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,22 +19,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
-    ImageView nav,compition,informals,games,highlights;
+    ImageView nav, compition, informals, games, highlights;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureNavigationDrawer();
         drawerLayout = findViewById(R.id.drawer_layout);
-        nav = (ImageView)findViewById(R.id.nav);
-        compition = (ImageView)findViewById(R.id.competitions);
-        informals = (ImageView)findViewById(R.id.informals);
-        games = (ImageView)findViewById(R.id.game);
-        highlights = (ImageView)findViewById(R.id.highlighgts);
+        nav = (ImageView) findViewById(R.id.nav);
+        compition = (ImageView) findViewById(R.id.competitions);
+        informals = (ImageView) findViewById(R.id.informals);
+        games = (ImageView) findViewById(R.id.game);
+        highlights = (ImageView) findViewById(R.id.highlighgts);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             NotificationManager mNotificationManager =
                     (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -46,36 +51,36 @@ public class MainActivity extends AppCompatActivity {
         compition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,competition.class);
+                Intent intent = new Intent(MainActivity.this, competition.class);
                 startActivity(intent);
-                overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                 finish();
             }
         });
         informals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,informals.class);
+                Intent intent = new Intent(MainActivity.this, informals.class);
                 startActivity(intent);
-                overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                 finish();
             }
         });
         games.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Games.class);
+                Intent intent = new Intent(MainActivity.this, Games.class);
                 startActivity(intent);
-                overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                 finish();
             }
         });
         highlights.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Highlights.class);
+                Intent intent = new Intent(MainActivity.this, Highlights.class);
                 startActivity(intent);
-                overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                 finish();
             }
         });
@@ -86,7 +91,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
     }
+
+    private void configureNavigationDrawer() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.nav_camera:
+                        Toast.makeText(MainActivity.this, "My Account", Toast.LENGTH_SHORT).show();
+                    case R.id.nav_gallery:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                    case R.id.nav_slideshow:
+                        Toast.makeText(MainActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                    case R.id.nav_manage:
+                        Toast.makeText(MainActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                    case R.id.nav_share:
+                        Toast.makeText(MainActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                    case R.id.nav_send:
+                        Toast.makeText(MainActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+            }
+        });
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
     protected void replaceContentLayout(int sourceId, int destinationId) {
         View contentLayout = findViewById(destinationId);
 
@@ -99,3 +139,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+
+
+
