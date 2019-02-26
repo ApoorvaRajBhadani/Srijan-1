68989c104ec306cbecea9e04a8843325e30b2e78
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-02-26 21:15:27 +0530

    developers ready

--- app/src/main/java/com/example/lenovo/srijan/Developers.java
+++ app/src/main/java/com/example/lenovo/srijan/Developers.java
@@ -1,7 +1,38 @@
 package com.example.lenovo.srijan;
 
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Developers extends AppCompatActivity {
 
+
+    private ArrayList<DeveloperModel> developersList = new ArrayList<>();
+    private ArrayList<DeveloperModel> designersList = new ArrayList<>();
+    private ListView developersListView;
+    private ListView designersListView;
+    private DeveloperAdapter developerAdapter;
+    private DeveloperAdapter designersAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers);
+        developersListView = findViewById(R.id.developers_listview);
+        designersListView = findViewById(R.id.designers_listview);
+
+        developersList.add(new DeveloperModel(R.drawable.ic_launcher_foreground,"Perul Jain","B.Tech. 2nd year","perul-jain-55845b154","https://github.com/peruljain","peruljain","perul.jain"));
+        developersList.add(new DeveloperModel(R.drawable.ic_launcher_foreground,"Perul Jain","B.Tech. 2nd year","perul-jain-55845b154","https://github.com/peruljain","peruljain","perul.jain"));
+        developersList.add(new DeveloperModel(R.drawable.ic_launcher_foreground,"Perul Jain","B.Tech. 2nd year","perul-jain-55845b154","https://github.com/peruljain","peruljain","perul.jain"));
+        designersList.add(new DeveloperModel(R.drawable.ic_launcher_foreground,"Perul Jain","B.Tech. 2nd year","perul-jain-55845b154","https://github.com/peruljain","peruljain","perul.jain"));
+        designersList.add(new DeveloperModel(R.drawable.ic_launcher_foreground,"Perul Jain","B.Tech. 2nd year","perul-jain-55845b154","https://github.com/peruljain","peruljain","perul.jain"));
+
+        developerAdapter = new DeveloperAdapter(Developers.this,developersList);
+        designersAdapter = new DeveloperAdapter(Developers.this,designersList);
+        developersListView.setAdapter(developerAdapter);
+        designersListView.setAdapter(designersAdapter);
+    }
 }
